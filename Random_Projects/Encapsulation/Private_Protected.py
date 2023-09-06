76b2d59b43ceda0ba07c5ab8d52fdba6b65f25bd
J0SH001 <gysljosh@gmail.com>
2023-09-06 17:10:49 -0500

    Updated

--- Random_Projects/Encapsulation/Private_Protected.py
+++ Random_Projects/Encapsulation/Private_Protected.py
@@ -2,5 +2,10 @@ class Protected_Private:
     def __init__(self):
         self._protectedVar = 24 #Protected variables have one prefixed underscore
         self.__privateVar = 27 #Private variables have two prefixed underscores
+    def getPrivate(self):
+        print(self.__privateVar)
 
+obj = Protected_Private()
+obj.getPrivate()
+print(obj._protectedVar)
 
